5783a9bddc2d4f94b716f9d23e57d4d1ca3912e0
Andrew <uytov1991@gmail.com>
2021-07-02 15:35:05 +0300

    fix tags popup

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -4,46 +4,50 @@ import { ReactComponent as Close } from './../img/svg/close.svg'
 import { ReactComponent as TagPlus } from './../img/svg/tag-plus.svg'
 import OutlineButton from './OutlineButton'
 import BackgroundButton from './BackgroundButton'
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef } from 'react'
 
-function renderPopupTag(tags, handleClickPopupTag) {
+function renderPopupTag(tags) {
   return tags.map((elem) => {
     let classes = elem.active
       ? 'tags-list__item-btn button active'
       : 'tags-list__item-btn button'
     return (
       <li className="tags-list__item" key={elem.value}>
-        <button onClick={handleClickPopupTag} className={classes}>
-          {elem.value}
-        </button>
+        <button className={classes}>{elem.value}</button>
       </li>
     )
   })
 }
 
 function TagPopup({ tags, handleClickSubmit, hidePopup }) {
-  const [popupTags, setPopupTags] = useState(tags)
-
-  // const prevTagsRef = useRef()
-  // useEffect(() => {
-  //   prevTagsRef.current = tags
-  // })
-  // const prevTags = prevTagsRef.current
-
+  const ulRef = useRef(null)
   const handleClickPopupTag = (evt) => {
-    const tag = evt.target.closest('button').textContent
-    const newTags = popupTags.map((elem) => {
-      if (elem.value === tag) {
-        elem.active = !elem.active
-      }
-      return elem
-    })
-
-    setPopupTags(newTags)
+    const tag = evt.target.closest('button')
+    tag.classList.toggle('active')
   }
 
   const handleClosePopup = () => {
     hidePopup()
+  }
+
+  const applyNewTags = () => {
+    const tags = ulRef.current.querySelectorAll('.tags-list__item-btn')
+    let result = []
+    tags.forEach((el) => {
+      result.push({
+        value: el.textContent,
+        active: el.classList.contains('active') ? true : false,
+      })
+    })
+
+    handleClickSubmit(result)
+  }
+
+  const resetTags = () => {
+    const tags = ulRef.current.querySelectorAll('.tags-list__item-btn')
+    tags.forEach((el) => {
+      el.classList.remove('active')
+    })
   }
 
   return (
@@ -56,19 +60,18 @@ function TagPopup({ tags, handleClickSubmit, hidePopup }) {
           <Close width="36" height="36" />
         </button>
         <h2 className="tags-popup__title">Выберите тэги</h2>
-        <ul className="tags-popup__list list">
-          {renderPopupTag(popupTags, handleClickPopupTag)}
+        <ul
+          ref={ulRef}
+          onClick={handleClickPopupTag}
+          className="tags-popup__list list"
+        >
+          {renderPopupTag(tags)}
         </ul>
         <div className="tags-popup__btns-wrapper">
-          <OutlineButton
-            onClick={() => setPopupTags([])}
-            classes="tags-popup__reset-btn"
-          >
+          <OutlineButton onClick={resetTags} classes="tags-popup__reset-btn">
             Отменить
           </OutlineButton>
-          <BackgroundButton onClick={() => handleClickSubmit(popupTags)}>
-            Применить
-          </BackgroundButton>
+          <BackgroundButton onClick={applyNewTags}>Применить</BackgroundButton>
         </div>
       </div>
     </div>
